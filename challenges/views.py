3efd53c8335edceb54b25a152b3b02e537131fba
Daniel Arias <darias1603@gmail.com>
2023-02-06 12:51:11 +0100

    Dynamic paths

--- challenges/views.py
+++ challenges/views.py
@@ -17,6 +17,20 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -24,13 +38,14 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month")
 
     redirect_month = months[month - 1]
-    redirect_path = reverse("month-challenge", args=[redirect_month])   # e.g. /challenge/january
+    redirect_path = reverse("month-challenge", args=[redirect_month])  # e.g. /challenge/january
     return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("This month is not found")
