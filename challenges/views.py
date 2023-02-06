2dae5c2233ed18663e8d450a83d4321f1440fdf5
Daniel Arias <darias1603@gmail.com>
2023-02-06 11:11:20 +0100

    Path Converters using Dictionary

--- challenges/views.py
+++ challenges/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 min every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Learn Django for at least 20 minutes every day!",
+    "may": "Walk for at least 20 min every day!",
+    "june": "Walk for at least 20 min every day!",
+    "july": "Walk for at least 20 min every day!",
+    "august": "Walk for at least 20 min every day!",
+    "october": "Walk for at least 20 min every day!",
+    "november": "Walk for at least 20 min every day!",
+    "december": "Walk for at least 20 min every day!"
+}
+
+
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
+
 
 def monthly_challenge(request, month):
-    match month:
-        case "january":
-            challenge_text = "Eat no meat for the entire month!"
-        case "february":
-            challenge_text = "Walk for at least 20 min every day!"
-        case "march":
-            challenge_text = "Learn Django for at least 20 minutes every day!"
-        case _:
-            return HttpResponseNotFound("This month is not supported!")
-
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("This month is not found")
